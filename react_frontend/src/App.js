3bf4337409565a6747bae95997409910917771ce
spirit-x-1 <spiritual_war_home@hotmail.com>
2024-06-01 20:43:10 +0100

    added loogo

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div>
       {/* Banner with space for logo */}
       <div style={{ backgroundColor: '#f0f0f0', padding: '20px' }}>
-        <img src="your_logo_url_here" alt="Logo" style={{ width: '200px', height: 'auto' }} />
+        <img src="/logo/sww_logo.png" alt="Logo" style={{ width: '200px', height: 'auto' }} />
       </div>
       
       {/* Main content */}
